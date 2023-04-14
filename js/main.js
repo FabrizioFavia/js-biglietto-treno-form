544e77b1737697aab4a8600655c449da636cf82e
FabrizioFavia <personal.favia@gmail.com>
2023-04-14 19:18:10 +0200

    feat(main): filled ticket

--- js/main.js
+++ js/main.js
@@ -2,19 +2,20 @@ function givePrice(event) {
 
     let kmPrice = 0.21;
     let totalPrice = null;
-    let messaggio = "Prezzo del biglietto: €";
+    let name = document.getElementById("ticketName").value;
     let age = document.getElementById("age").value;
     let distance = document.getElementById("distance").value;
     totalPrice = (distance * kmPrice).toFixed(2);
+    document.getElementById("name").innerText = `${name.toUpperCase()}`;
     if (age < 18) {
         let underDiscount = (totalPrice - (totalPrice * 20 / 100)).toFixed(2);
-        document.getElementById("price").innerText = (`${messaggio} ${underDiscount}`);
+        document.getElementById("price").innerText = "€" (`${underDiscount}`);
     }
     else if (age > 65) {
         let overDiscount = (totalPrice - (totalPrice * 40 / 100)).toFixed(2);
-        document.getElementById("price").innerText = (`${messaggio} ${overDiscount}`);
+        document.getElementById("price").innerText = (`${overDiscount}`);
     }
     else {
-        document.getElementById("price").innerText = (`${messaggio} ${totalPrice}`);
+        document.getElementById("price").innerText = (` ${totalPrice}`);
     }
 }
