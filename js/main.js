b99924831e09b1cfe0e55574aad1f3c75ef05f8c
FabrizioFavia <personal.favia@gmail.com>
2023-04-17 11:07:18 +0200

    correzione lezione

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 function giveTicket(event) {
 
     let kmPrice = 0.21;
-    let totalPrice = null;
+    var totalPrice = null;
     var message = "Biglietto Standard"
     var message20 = "Sconto 20%";
     var message40 = "Sconto 40%";
     let name = document.getElementById("ticketName").value;
-    let distance = document.getElementById("distance").value;
+    let distance = Number(document.getElementById("distance").value);
     let age = document.getElementById("age").value;
     let coach = Math.floor(Math.random() * 10) + 1;
     let cpCode = Math.floor(Math.random() * 10000);
@@ -16,12 +16,12 @@ function giveTicket(event) {
     document.getElementById("cpCode").innerText = cpCode;
 
     if (age < 18) {
-        let underDiscount = (totalPrice - (totalPrice * 20 / 100)).toFixed(2);
+        var underDiscount = (totalPrice - (totalPrice * 20 / 100)).toFixed(2);
         document.getElementById("price").innerText = (`${underDiscount} €`);
         document.getElementById("ticketType").innerText = (` ${message20}`);
     }
     else if (age > 65) {
-        let overDiscount = (totalPrice - (totalPrice * 40 / 100)).toFixed(2);
+        var overDiscount = (totalPrice - (totalPrice * 40 / 100)).toFixed(2);
         document.getElementById("price").innerText = (`${overDiscount} €`);
         document.getElementById("ticketType").innerText = (` ${message40}`);
     }
@@ -31,69 +31,62 @@ function giveTicket(event) {
     }
 };
 
+function reset(){
+    let nome = document.getElementById("ticketName");
+    let distance = document.getElementById("distance")
+    let age = document.getElementById("age")
+    nome.value = " ";
+    distance.value = " ";
+    age.value = " ";
+}
+
 function whiteLight() {
 
     var yellowLightBulb = document.getElementById("yellowLightBulb");
-    yellowLightBulb.classList.add("hidden");
-
     var body = document.getElementById("body");
-    body.classList.add("bg-white");
-
     var paperless = document.getElementById("paperless");
-    paperless.classList.add("text-warning");
-
     var logo = document.getElementById("logo");
-    logo.classList.add("text-warning");
-
     var home = document.getElementById("home");
-    home.classList.add("text-warning");
-
     var features = document.getElementById("features");
-    features.classList.add("text-warning");
-
     var pricing = document.getElementById("pricing");
-    pricing.classList.add("text-warning");
-
     var navbar = document.getElementById("navbar");
-    navbar.classList.add("border-bottom");
-
     var navbar = document.getElementById("navbar");
-    navbar.classList.add("border-2");
-
     var navbar = document.getElementById("navbar");
-    navbar.classList.add("myLightBlackBck");
-
     var form = document.getElementById("form");
-    form.classList.add("border");
-
     var form = document.getElementById("form");
-    form.classList.add("border-2");
-
     var ticket = document.getElementById("ticket");
-    ticket.classList.add("border");
-
-    var ticket = document.getElementById("ticket");
-    ticket.classList.add("border-2");
-
     const ticketSpace = document.querySelectorAll(".ticketSpace");
+    var passengerName = document.getElementById("passengerName");
+    var ticketSale = document.getElementById("ticketSale");
+    var coachTitle = document.getElementById("coachTitle");
+    var codeTitle = document.getElementById("codeTitle");
+    var ticketPrice = document.getElementById("ticketPrice");
+
+
+    yellowLightBulb.classList.add("hidden");
+    body.classList.add("bg-white");
+    paperless.classList.add("text-warning");
+    logo.classList.add("text-warning");
+    home.classList.add("text-warning");
+    features.classList.add("text-warning");
+    pricing.classList.add("text-warning");
+    navbar.classList.add("border-bottom");
+    navbar.classList.add("border-2");
+    navbar.classList.add("myLightBlackBck");
+    form.classList.add("border");
+    form.classList.add("border-2");
+    ticket.classList.add("border");
+    ticket.classList.add("border-2");
+    passengerName.classList.add("text-dark");
+    ticketSale.classList.add("text-dark");
+    coachTitle.classList.add("text-dark");
+    codeTitle.classList.add("text-dark");
+    ticketPrice.classList.add("text-dark");
+    
     for (let i = 0; i < ticketSpace.length; i++) {
         ticketSpace[i].style.backgroundColor = "white";
     };
 
-    var passengerName = document.getElementById("passengerName");
-    passengerName.classList.add("text-dark");
-
-    var ticketSale = document.getElementById("ticketSale");
-    ticketSale.classList.add("text-dark");
-
-    var coachTitle = document.getElementById("coachTitle");
-    coachTitle.classList.add("text-dark");
-
-    var codeTitle = document.getElementById("codeTitle");
-    codeTitle.classList.add("text-dark");
-
-    var ticketPrice = document.getElementById("ticketPrice");
-    ticketPrice.classList.add("text-dark");
 }
 
 
